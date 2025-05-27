25612c852d36423bce9eef4868db8a091e01d5d7
NickJones <n1ch0l45j0n35@gmail.com>
2025-05-27 15:53:57 -0600

    Began adding stub methods to dataaccess sql classes

--- server/src/main/java/dataaccess/sql/SQLUserDAO.java
+++ server/src/main/java/dataaccess/sql/SQLUserDAO.java
@@ -1,4 +1,23 @@
 package dataaccess.sql;
 
-public class SQLUserDAO {
+import dataaccess.UserDAO;
+import model.UserData;
+
+public class SQLUserDAO implements UserDAO {
+
+    @Override
+    public void createUser(UserData user) {
+        // TODO = SQL INSERT INTO users
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        // TODO = SQL SELECT FROM users WHERE username = x
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO = Implement SQL DELETE FROM users
+    }
 }
