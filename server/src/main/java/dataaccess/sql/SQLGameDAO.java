9b27444574fdd232bfde0d1cb2ae10254ce04c35
NickJones <n1ch0l45j0n35@gmail.com>
2025-05-27 19:27:52 -0600

    Added stubs to SQLGameDAO that match GameDAO

--- server/src/main/java/dataaccess/sql/SQLGameDAO.java
+++ server/src/main/java/dataaccess/sql/SQLGameDAO.java
@@ -1,4 +1,41 @@
 package dataaccess.sql;
 
-public class SQLGameDAO {
+import com.google.gson.Gson;
+import dataaccess.GameDAO;
+import model.GameData;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SQLGameDAO implements GameDAO {
+
+    private final Gson gson = new Gson();
+
+    @Override
+    public int createGame(String gameName) {
+        // TODO: INSERT INTO games (gameName, game) VALUES (?, ?)
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        // TODO: SELECT * FROM games WHERE id = ?
+        return null;
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+        // TODO: UPDATE games SET game=?, whitePlayer=?, blackPlayer=? WHERE id=?
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        // TODO: SELECT * FROM games
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void clear() {
+        // TODO: DELETE FROM games
+    }
 }
