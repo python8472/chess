043daa6b8eb64a83a02146a2fab93063788d02ae
NickJones <n1ch0l45j0n35@gmail.com>
2025-05-27 19:22:51 -0600

    Added stubs to SQLAuthDAO that match AuthDAO

--- server/src/main/java/dataaccess/sql/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/sql/SQLAuthDAO.java
@@ -1,4 +1,38 @@
 package dataaccess.sql;
 
-public class SQLAuthDAO {
+import dataaccess.AuthDAO;
+import model.AuthData;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class SQLAuthDAO implements AuthDAO {
+
+    @Override
+    public AuthData createAuth(String username) {
+        // TODO = Generate token, store (token, username), and return AuthData
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        // TODO = SELECT * FROM auth_tokens WHERE token = ?
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        // TODO = DELETE FROM auth_tokens WHERE token = ?
+    }
+
+    @Override
+    public void clear() {
+        // TODO = DELETE FROM auth_tokens
+    }
+
+    @Override
+    public Collection<String> getAllAuthTokens() {
+        // TODO = SELECT token FROM auth_tokens
+        return Collections.emptyList();
+    }
 }
