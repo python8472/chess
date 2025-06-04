d107285c64193b15d2eef50c067630b7e159d5d6
NickJones <n1ch0l45j0n35@gmail.com>
2025-06-03 18:27:33 -0600

    Made a helper class for Pre and Post Login because both use it. Added text and method explanations using the escape sequences

---
+++ client/src/main/java/ui/HelpHelper.java
@@ -0,0 +1,32 @@
+package ui;
+
+import static ui.EscapeSequences.*;
+
+public class HelpHelper {
+
+    public static void printPreLoginHelp() {
+        System.out.println(SET_TEXT_COLOR_BLUE + SET_TEXT_BOLD + "Available commands (Pre-Login):" + RESET_TEXT_COLOR + RESET_TEXT_BOLD_FAINT);
+        System.out.println(SET_TEXT_COLOR_GREEN + "  help" + RESET_TEXT_COLOR + "                           Show helpful commands");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  quit" + RESET_TEXT_COLOR + "                           Quit the app");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  register" + RESET_TEXT_COLOR + " <user> <pass> <email>     Register a new user");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  login" + RESET_TEXT_COLOR + "    <user> <pass>            Login as an existing user");
+    }
+
+    public static void printPostLoginHelp() {
+        System.out.println(SET_TEXT_COLOR_BLUE + SET_TEXT_BOLD + "Available commands (Post-Login):" + RESET_TEXT_COLOR + RESET_TEXT_BOLD_FAINT);
+        System.out.println(SET_TEXT_COLOR_GREEN + "  help" + RESET_TEXT_COLOR + "                          Show helpful commands");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  logout" + RESET_TEXT_COLOR + "                        Log out");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  create" + RESET_TEXT_COLOR + " <game-name>             Create a new game");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  list" + RESET_TEXT_COLOR + "                          List all games");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  join" + RESET_TEXT_COLOR + " <game-id> <WHITE|BLACK|empty>  Join a game");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  observe" + RESET_TEXT_COLOR + " <game-id>              Observe a game");
+    }
+
+    public static void printGameplayHelp() {
+        System.out.println(SET_TEXT_COLOR_BLUE + SET_TEXT_BOLD + "Available commands (Gameplay):" + RESET_TEXT_COLOR + RESET_TEXT_BOLD_FAINT);
+        System.out.println(SET_TEXT_COLOR_GREEN + "  help" + RESET_TEXT_COLOR + "                          Show helpful commands");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  move" + RESET_TEXT_COLOR + " <from> <to> [promo]      Make a move");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  redraw" + RESET_TEXT_COLOR + "                        Redraw the board if needed");
+        System.out.println(SET_TEXT_COLOR_GREEN + "  leave" + RESET_TEXT_COLOR + "                         Leave the game");
+    }
+}
